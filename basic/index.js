fa96503d1871f312a8778abb2aa2b770751d3bb6
Rory Ashford <labs@codebluedigital.com>
2016-07-01 11:23:05 +0100

    Init commit

---
+++ basic/index.js
@@ -0,0 +1,11 @@
+/**
+ * React and such
+ */
+
+ var React = require('react');
+ var ReactDOM = require('react-dom');
+
+ ReactDOM.render(
+   <h1>Hello, world!</h1>,
+   document.getElementById('example')
+ );
